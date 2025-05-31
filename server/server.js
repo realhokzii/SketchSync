3c01a904fb26f3b2561410b8f513b1e4b3be4b30
Gabriel Malabanan <gabmalabanan28@gmail.com>
2025-05-30 22:05:04 -0400

    Create server.js with basic Express server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
 
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: '*' }
+});
+
+app.use(cors());
+app.get('/', (req, res) => res.send('Server is running!'));
+
+server.listen(3001, () => console.log('Server on http://localhost:3001'));
